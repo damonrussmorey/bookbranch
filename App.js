b7ea5e6dfc82dd4e4e783e2c37feb959e672c155
Damon Morey <dmorey@pdx.edu>
2019-05-06 20:31:10 -0700

    Hello World
    Make sure you can run a simulator.

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button style={styles.instructions} title={buttonText} onPress={updateButton}></Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#23FF23',
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#0000FF',
+    margin: 30
+  },
+  instructions: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#FF00FF',
+    marginBottom: 20,
+  },
+});
---
+++ run.bat
@@ -0,0 +1,2 @@
+start /b "" %ANDROID_HOME%\emulator\emulator.exe -avd Pixel_2_API_28
+start /b "" react-native run-android
