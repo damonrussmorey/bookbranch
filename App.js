1d8c257d8109bc616fa5279fc3697cc8c104a27b
Damon Morey <dmorey@pdx.edu>
2019-05-13 22:19:56 -0700

    Fix hopefully

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,16 +16,14 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-var clickCount = 0;
-var buttonText = 'clicky!';
-export default class App extends Component {
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <Button title={buttonText} onPress={function rawr() {buttonText=clickCount++;}}></Button>
       </View>
     );
   }
@@ -36,18 +34,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#23FF23',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 40,
-    textAlign: 'center',
-    color: '#0000FF',
-    margin: 30
-  },
-  instructions: {
     fontSize: 20,
     textAlign: 'center',
-    color: '#FF00FF',
-    marginBottom: 20,
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
