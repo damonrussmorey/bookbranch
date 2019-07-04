a6fdbb7fc4863d04c6829615cf2799dd54b51407
dmesa2 <dmesa2@pdx.edu>
2019-07-04 13:26:29 -0700

    Finished attributes list

--- src/components/ChooseAttributeList.js
+++ src/components/ChooseAttributeList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image} from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import AttributesDetail from './AttributesDetail';
 import Card from './Card';
 import AttributeCard from './AttributeCard';
@@ -15,12 +15,269 @@ class AttributesList extends Component {
         return (
             <View>
                 <Card>
-                    <AttributeCard>
-                        <Image
-                             style={styles.imageStyle}
-                             source={require('bookbranch/img/adventurous-attribute.png')}>
-                        </Image>
-                    </AttributeCard>
+                    <ScrollView>
+
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/adventurous-attribute.png')}>
+                            </Image>
+                            <Text>Adventurous</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/beautiful-attribute.png')}>
+                            </Image>
+                            <Text>Beautiful</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/brainy-attribute.png')}>
+                            </Image>
+                            <Text>Brainy</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                            +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/complex-attribute.png')}>
+                            </Image>
+                            <Text>Complex</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/cooking-attribute.png')}>
+                            </Image>
+                            <Text>Cooking</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                        +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/cultural-attribute.png')}>
+                            </Image>
+                            <Text>Cultural</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                         +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/dark-attribute.png')}>
+                            </Image>
+                            <Text>Dark</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                               +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/disaster-attribute.png')}>
+                            </Image>
+                            <Text>Disaster</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                        +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/erotic-attribute.png')}>
+                            </Image>
+                            <Text>Erotic</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                             +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/faith-attribute.png')}>
+                            </Image>
+                            <Text>Faith</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                               +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/family-attribute.png')}>
+                            </Image>
+                            <Text>Family</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                            +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/fantasy-attribute.png')}>
+                            </Image>
+                            <Text>Fantasy</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                          +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/friendship-attribute.png')}>
+                            </Image>
+                            <Text>Friendship</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                     +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/funny-attribute.png')}>
+                            </Image>
+                            <Text>Funny</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                             +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/heroic-attribute.png')}>
+                            </Image>
+                            <Text>Heroic</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                            +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/historical-attribute.png')}>
+                            </Image>
+                            <Text>Historical</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/idealistic-attribute.png')}>
+                            </Image>
+                            <Text>Idealistic</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                        +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/insightful-attribute.png')}>
+                            </Image>
+                            <Text>Insightful</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/knowledge-attribute.png')}>
+                            </Image>
+                            <Text>Knowledge</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                    +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/mysterious-attribute.png')}>
+                            </Image>
+                            <Text>Mysterious</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                    +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/perserverence-attribute.png')}>
+                            </Image>
+                            <Text>Perserverence</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>              +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/power-attribute.png')}>
+                            </Image>
+                            <Text>Power</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                             +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/readable-attribute.png')}>
+                            </Image>
+                            <Text>Readable</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/romantic-attribute.png')}>
+                            </Image>
+                            <Text>Romantic</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                       +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/scary-attribute.png')}>
+                            </Image>
+                            <Text>Scary</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                              +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <AttributeCard>
+                            <Image
+                                style={styles.imageStyle}
+                                source={require('bookbranch/img/attributes/suspenseful-attribute.png')}>
+                            </Image>
+                            <Text>Suspenseful</Text>
+                            <Text style = {{fontWeight: 'bold', color: '#499920'}}>                  +</Text>
+                        </AttributeCard>
+                    </TouchableOpacity>
+                    </ScrollView>
                 </Card>
             </View>
         );
