b0d694e8055ba1cfb29778ea5f49fbc618120a1f
Damon Morey <dmorey@pdx.edu>
2019-07-08 20:03:11 -0700

    Different mysql library so connections return a promise; updated recommendation to return a list

--- src/backend/recommendation.js
+++ src/backend/recommendation.js
@@ -26,8 +26,8 @@ We need the book to skip it from matching to itself
 
 This can be used without needing to exclude a book, just send book_id: -1.
 */
-module.exports = (pool, req, res) => {
-  console.log('\n\nBook Recommendation\n')
+module.exports = async (pool, req, res) => {
+  console.log('\n\nBook Recommendation')
 
   //sort attr ids in ascending order
   req.body.attr.sort((a,b) => {
@@ -46,47 +46,68 @@ module.exports = (pool, req, res) => {
   }
 
   //display the adjusted request for testing
-  console.log('Request Body:\n' + req.body);
+  console.log('Request Body:\n' + JSON.stringify(req.body));
 
-  var bestMatch = {id:-1, distance:-1};
-  pool.getConnection((err, connection) => {
-    //For each book in the database, find the euclidean distance
-    //for the 3 attributes given, ignore the other attributes
-    //if no attribute score for a book exists, call its value 0
-
-    //match book ids and attribute ids to get average_score,
-    //which is an average of all the attributes scores for all
-    //the reviews
-
-    connection.query('SELECT book_id, attribute_id, average_score FROM '
-      + 'book_attributes WHERE NOT book_id = ' + req.body.book_id
-      + ' AND (attribute_id = ' + req.body.attr[0].id
+  const PriorityQueue = require('js-priority-queue');
+  let connection,
+      result,
+      queue = new PriorityQueue({
+        comparator: (a, b) => { return a.distance - b.distance; }}),
+      i, j, x, book_id;
+  try {
+    connection = await pool.getConnection();
+    result = await connection.query(
+        'SELECT book_id, attribute_id, average_score FROM '
+        + 'book_attributes WHERE NOT book_id = ' + req.body.book_id
+        + ' AND (attribute_id = ' + req.body.attr[0].id
         + ' OR attribute_id = ' + req.body.attr[1].id
         + ' OR attribute_id = ' + req.body.attr[2].id
-        + ') ORDER BY book_id, attribute_id;',
-      (err, result) => {
-        var i = 0, j, x;
-        while(i < result.length) {
-          var book_id = result[i].book_id;
-          x = j = 0;
-          while(i < result.length && result[i].book_id == book_id) {
-            while(result[i].attribute_id > req.body.attr[j].id)
-              x += Math.pow(req.body.attr[j++].value, 2);
-            x += Math.pow(result[i].average_score - req.body.attr[j].value, 2);
-            ++i;
-            ++j;
-          }
-          while(j < req.body.attr.length)
-            x += Math.pow(req.body.attr[j++].value, 2);
-          x = Math.sqrt(x);
-          if(bestMatch.id == -1 || x < bestMatch.distance) {
-            console.log(`New best: distance from ${result[i-1].book_id}: ${x}`)
-            bestMatch.distance = x;
-            bestMatch.id = result[i-1].book_id
-          }
-        }    
-      //return the best match
-      res.send({book_id: bestMatch.id});
-    });
-  });
+        + ') ORDER BY book_id, attribute_id;');
+    result = result[0];
+    if(!result) {
+      throw 'no matches';
+    }
+    if(!Array.isArray(result)) {
+      result = [result];
+    }
+  } finally {
+    if(connection && connection.release)  connection.release();
+  }
+
+  //For each book in the database, find the euclidean distance
+  //for the 3 attributes given, ignore the other attributes
+  //if no attribute score for a book exists, call its value 0
+
+  //match book ids and attribute ids to get average_score,
+  //which is an average of all the attributes scores for all
+  //the reviews
+
+  i = 0;
+  while(i < result.length) {
+    book_id = result[i].book_id;
+    x = j = 0;
+    while(i < result.length && result[i].book_id == book_id) {
+      while(result[i].attribute_id > req.body.attr[j].id)
+        x += Math.pow(req.body.attr[j++].value, 2);
+      x += Math.pow(result[i].average_score - req.body.attr[j].value, 2);
+      ++i;
+      ++j;
+    }
+    while(j < req.body.attr.length)
+      x += Math.pow(req.body.attr[j++].value, 2);
+    x = Math.sqrt(x);
+    //console.log(`${book_id}: ${x}`);
+    queue.queue({id: book_id, distance: x});
+  }    
+
+  //return top 50 matches, within 10 distance
+  result = []
+  for(i = 0; i < 50; ++i) {
+    x = queue.dequeue();
+    //console.log(x);
+    if(x.distance > 10)
+      break;
+    result.push(x);
+  }
+  res.send(result);
 }
--- src/backend/test_recommendation.js
+++ src/backend/test_recommendation.js
@@ -8,39 +8,46 @@ async function test(test, body) {
         body: JSON.stringify(body)
     });
     var res = await hi.json();
-    console.log(test + ': ', res.book_id);
+    console.log(test + ': ', JSON.stringify(res));
+
 }
 
+(async () => {
+  //test 1: send a direct match (1101)
+  //leave attr ids unsorted to ensure they sort properly
+  test('test 1', {
+      book_id: 1,
+      attr: [
+          {id: 1, value: 3},
+          {id: 13, value: 8},
+          {id: 2, value: 4}
+      ]
+  });
 
-//test 1: send a direct match (1101)
-//leave attr ids unsorted to ensure they sort properly
-test('test 1', {
-    book_id: 1,
-    attr: [
-        {id: 1, value: 3},
-        {id: 13, value: 8},
-        {id: 2, value: 4}
-    ]
-});
+  await new Promise(done => setTimeout(done, 3000));
 
-//test 2: send a direct match, but exclude that book
-//see what the next closest match is
-test('test 2', {
-    book_id: 1101,
-    attr: [
-        {id: 1, value: 3},
-        {id: 13, value: 8},
-        {id: 2, value: 4}
-    ]
-});
+  //test 2: send a direct match, but exclude that book
+  //see what the next closest match is
+  test('test 2', {
+      book_id: 1101,
+      attr: [
+          {id: 1, value: 3},
+          {id: 13, value: 8},
+          {id: 2, value: 4}
+      ]
+  });
 
-//test 3: send bad attribute values, see if they clamp
-//also send "no" book
-test('test 3', {
-    book_id: -1,
-    attr: [
-        {id: -2, value: 3},
-        {id: 1, value: -2},
-        {id: 2, value: 20}
-    ]
-});
+  await new Promise(done => setTimeout(done, 3000));
+
+  //test 3: send bad attribute values, see if they clamp
+  //also send "no" book
+  test('test 3', {
+      book_id: -1,
+      attr: [
+          {id: -2, value: 3},
+          {id: 1, value: -2},
+          {id: 2, value: 20}
+      ]
+  });
+
+})();
