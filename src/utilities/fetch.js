affb85b3b0d2b0b174be58156764e3fa538b6350
Damon Morey <dmorey@pdx.edu>
2019-07-22 18:11:43 -0700

    Untested authenicate utility

--- src/utilities/fetch.js
+++ src/utilities/fetch.js
@@ -1,26 +1,28 @@
 /*
 Fetch wrapper for front end
 */
-module.exports.get = async (route) => {
-  let res = await fetch('http://localhost:8765/' + route);
-  res = await res.JSON();
-  return res;
-}
+export default class Backend {
+  async function get(route) {
+    let res = await fetch('http://localhost:8765/' + route);
+    res = await res.JSON();
+    return res;
+  }
 
-module.exports.post = async (route, body) => {
-  let res = await fetch('http://localhost:8765/' + route, {
-    headers: {
-        'content-type': 'application/json',
-        Accept: 'application/json'
-    },
-    method: 'POST',
-    body: JSON.stringify(body)
-  });
-  res = await res.JSON();
-  return res;
+  async function post(route, body) {
+    let res = await fetch('http://localhost:8765/' + route, {
+      headers: {
+          'content-type': 'application/json',
+          Accept: 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(body)
+    });
+    res = await res.JSON();
+    return res;
+  }
 }
 
 /*example usage
-const backend = require('../utilities/fetch')
-let attr = await backend.get('attributes');
-let book_info = await backend.post('keyword', {keyword_search = 'the hunger games'});
+import Backend from '../utilities'
+let attr = await Backend.get('attributes');
+let book_info = await Backend.post('keyword', {keyword_search = 'the hunger games'});
