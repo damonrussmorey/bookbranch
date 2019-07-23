c09b583407d01534018d003f58943215f60c333b
Damon Morey <dmorey@pdx.edu>
2019-07-22 19:41:18 -0700

    Update to recommendation to support multiple books

--- src/backend/recommendation.js
+++ src/backend/recommendation.js
@@ -5,32 +5,37 @@ Returns the book_id of the recommended book
 
 Body of request:
 {
-  book_id: int (optional)
-  attr:
+  books:
+  [
+    int,
+    ...
+  ]
+  attrs:
   [
     {
       id: int
       val: int [1-10]
     }
-  ] //up to 3?
+  ]
 }
 
 Body of response:
-{
-  book_id: int
-}
+[
+  int,
+  ...
+]
 
-Assume the names of the attributes are correct
+Assume the ids of the attributes are correct
 Attribute values are integers from 1 to 10 inclusive
-We need the book to skip it from matching to itself
+We need the books to skip it from matching to itself
 
 This can be used without needing to exclude a book, just send book_id: -1.
 */
 module.exports = async (pool, req, res) => {
-  console.log('\n\nBook Recommendation')
+  console.log('\nBook Recommendation')
 
   //sort attr ids in ascending order
-  req.body.attr.sort((a,b) => {
+  req.body.attrs.sort((a,b) => {
     if(a.id > b.id) return 1;
     else  return -1;
   })
@@ -38,15 +43,11 @@ module.exports = async (pool, req, res) => {
   //ensure proper attribute values
   //invalid ids will simply not match
   //invalid values will mess up distance calculation, so clamp them
-  for(attr of req.body.attr) {
+  for(attr of req.body.attrs) {
     if(attr.value < 1)
       attr.value = 1;
     if(attr.value > 10)
       attr.value = 10;
-  }
-
-  if(!req.body.book_id) {
-    req.body.book_id = -1;
   }
 
   //debug
@@ -60,13 +61,24 @@ module.exports = async (pool, req, res) => {
       i, j, x, book_id, query;
   try {
     connection = await pool.getConnection();
+
     query = 'SELECT book_id, attribute_id, average_score FROM '
-        + 'book_attributes WHERE NOT book_id=' + req.body.book_id
-        + ' AND (';
-    for(attr of req.body.attr) {
+        + 'book_attributes WHERE ';
+    
+    if(req.body.books) {
+      query += 'NOT (';
+      for(b of req.body.books) {
+        query += 'book_id=' + b + ' AND ';
+      }
+      query = query.slice(0, -5) + ') AND';
+    }
+
+    query += '(';
+    for(attr of req.body.attrs) {
       query += 'attribute_id=' + attr.id + ' OR ';
     }
     query = query.slice(0, -4) + ') ORDER BY book_id, attribute_id;'
+
     result = await connection.query(query);
     result = result[0];
     //debug
@@ -95,18 +107,18 @@ module.exports = async (pool, req, res) => {
     book_id = result[i].book_id;
     x = j = 0;
     while(i < result.length && result[i].book_id == book_id) {
-      while(result[i].attribute_id > req.body.attr[j].id)
-        x += Math.pow(req.body.attr[j++].value, 2);
-      x += Math.pow(result[i].average_score - req.body.attr[j].value, 2);
+      while(result[i].attribute_id > req.body.attrs[j].id)
+        x += Math.pow(req.body.attrs[j++].value, 2);
+      x += Math.pow(result[i].average_score - req.body.attrs[j].value, 2);
       ++i;
       ++j;
     }
-    while(j < req.body.attr.length)
-      x += Math.pow(req.body.attr[j++].value, 2);
-    x = Math.sqrt(x) / req.body.attr.length;
+    while(j < req.body.attrs.length)
+      x += Math.pow(req.body.attrs[j++].value, 2);
+    x = Math.sqrt(x) / req.body.attrs.length;
     //console.log(`${book_id}: ${x}`);
     queue.queue({id: book_id, distance: x});
-  }    
+  }
 
   //return top 50 matches, within 5 distance
   result = []
@@ -138,8 +150,8 @@ if(process.argv[2] === 'test') {
     //test 1: send a direct match (1101)
     //leave attr ids unsorted to ensure they sort properly
     test('test 1', {
-      book_id: 1,
-      attr: [
+      books: [1],
+      attrs: [
         {id: 1, value: 3},
         {id: 13, value: 8},
         {id: 2, value: 4}
@@ -151,8 +163,8 @@ if(process.argv[2] === 'test') {
     //test 2: send a direct match, but exclude that book
     //see what the next closest match is
     test('test 2', {
-      book_id: 1101,
-      attr: [
+      books: [1101],
+      attrs: [
         {id: 1, value: 3},
         {id: 13, value: 8},
         {id: 2, value: 4}
@@ -162,9 +174,9 @@ if(process.argv[2] === 'test') {
     await new Promise(done => setTimeout(done, 3000));
 
     //test 3: send bad attribute values, see if they clamp
-    //also send no book
+    //also send no books
     test('test 3', {
-      attr: [
+      attrs: [
         {id: -2, value: 3},
         {id: 1, value: -2},
         {id: 2, value: 20}
@@ -175,7 +187,23 @@ if(process.argv[2] === 'test') {
 
     //test 4: send 8 attribute values
     test('test 4', {
-      attr: [
+      attrs: [
+        {id: 1, value: 3},
+        {id: 2, value: 4},
+        {id: 3, value: 5},
+        {id: 4, value: 6},
+        {id: 5, value: 7},
+        {id: 6, value: 8},
+        {id: 7, value: 10},
+        {id: 8, value: 1},
+      ]
+    });
+
+    await new Promise(done => setTimeout(done, 3000));
+
+    test('test 5', {
+      books: [10,20,30,40,50,60,70],
+      attrs: [
         {id: 1, value: 3},
         {id: 2, value: 4},
         {id: 3, value: 5},
@@ -188,3 +216,4 @@ if(process.argv[2] === 'test') {
     });
   })();
 }
+
