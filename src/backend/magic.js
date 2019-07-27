4ba90d7968d008d87eb568f6eff8e3b916e97a6c
boxuan <boxuan@pdx.edu>
2019-07-26 18:14:44 -0700

    fix and wait to review

--- src/backend/magic.js
+++ src/backend/magic.js
@@ -58,21 +58,25 @@ module.exports = async (pool, req, res) => {
     attr1.push({id: req.body.attr1id2, val: req.body.attr1val2});
   if(attr1.filter(e => e.id === req.body.attr1id3).length == 0)
     attr1.push({id: req.body.attr1id3, val: req.body.attr1val3});
-
+  console.log("######Book 1 track begin ######################")
   //find book 1 on AWS
-  book1 = require('./find_book')(pool, req.body.title1);
+  var name = req.body.title1;
+  book1 = await require('./find_book')(name);
+  console.log(book1);
 
   //add book 1 to database (or simply return the id if its already there)
-  book1_id = require('./insert_new_book')(pool, book1);
-
+  book1_id = await require('./insert_new_book')(pool, book1);
+  console.log(book1_id)
   //add book 1 review to database
-  success = require('./insert_recommandation_review')(pool, {
+  success = await require('./insert_recommandation_review')(pool, {
     book_id: book1_id,
     attr: attr1,
     user_id: req.body.user_id,
     rating_value: req.body.rating1
   });
-    
+  console.log("############book 1 track end ##################")
+  
+  
   //remove duplicate attributes for book 2
   let attr2 = [{id: req.body.attr2id1, val: req.body.attr2val1}];
   if(attr2.filter(e => e.id === req.body.attr2id2).length == 0)
@@ -80,19 +84,22 @@ module.exports = async (pool, req, res) => {
   if(attr2.filter(e => e.id === req.body.attr2id3).length == 0)
     attr2.push({id: req.body.attr2id3, val: req.body.attr2val3});
 
+  console.log("\n\n###############book 2 track begin############")
   //find book 2 on AWS
-  book2 = require('./find_book')(pool, req.body.title2);
-
+  var name = req.body.title2;
+  book2 = await require('./find_book')(name);
+  console.log(book2)
   //add book 2 to database (or simply return the id if its already there)
-  book2_id = require('./insert_new_book')(pool, book2);
+  book2_id = await require('./insert_new_book')(pool, book2);
 
   //add book 2 review to database
-  success = require('./insert_recommandation_review')(pool, {
+  success = await require('./insert_recommandation_review')(pool, {
     book_id: book2_id,
     attr: attr2,
     user_id: req.body.user_id,
     rating_value: req.body.rating2
   });
+  console.log("###################Book 2 track end ##############")
 
   //fill out attribute list with only unique attributes, average
   //them if they are the same
@@ -129,13 +136,14 @@ module.exports = async (pool, req, res) => {
   
   //Send the results back to the client
   res.send(recommendations);
+
 }
 
 //test
 if(process.argv[2] === 'test') {
   const fetch = require('node-fetch');
   (async () => {
-    let hi = await fetch('http://localhost:8765/recommendation', {
+    let hi = await fetch('http://localhost:8765/magic', {
       headers: {
         'content-type': 'application/json',
         Accept: 'application/json'},
@@ -144,7 +152,7 @@ if(process.argv[2] === 'test') {
         title1:       "joan of arc",
         rating1:      4,
         attr1id1:     1,
-        attr1val1     10,
+        attr1val1:     10,
         attr1id2:     5,
         attr1val2:    8,
         attr1id3:     18,
@@ -152,12 +160,12 @@ if(process.argv[2] === 'test') {
         title2:       "albert einstein biography",
         rating2:      5,
         attr2id1:     2,
-        attr2val1     8,
+        attr2val1:     8,
         attr2id2:     5,
         attr2val2:    10,
         attr2id3:     20,
         attr2val3:    7,
-        user_id:      200
+        user_id:      1
       })
     });
     let res = await hi.json();
