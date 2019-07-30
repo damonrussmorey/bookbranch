411b497566ea6f1fc2b3114b9a8a65fd52b42098
Austin Parrish <paustin@raccoonserver.net>
2019-07-29 18:38:34 -0700

    working with async example

--- src/backend/create_hash.js
+++ src/backend/create_hash.js
@@ -1,11 +1,28 @@
 /*
 Create Hash
-Creates a hash that the system can understand
+Creates a hash that works with laravel backend
 */
-
 const bcrypt = require('bcrypt');
-const myPlaintextPassword = 'bcrypt1999';
 
-bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
-    console.log(hash.replace(/^.{3}/g, '$2y'));
+const saltPasswordAsync = (password, rounds) => new Promise((resolve, reject) => {
+    bcrypt.hash(password, rounds, (err, hash) => {
+        if (err) {
+            reject(err);
+        }
+        else{
+            resolve(hash.replace(/^.{3}/g, '$2y'));
+        }
+    });
 });
+
+// example
+async function hashPassword(plainPassword) {
+    try {
+        const hash = await saltPasswordAsync(plainPassword, 10);
+        console.log('The hash is', hash);
+    } catch (err) {
+        console.error('There was an error ', err);
+    }
+}
+
+hashPassword("bcrypt1999");
