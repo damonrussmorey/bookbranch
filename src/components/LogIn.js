4ce3c435a3f654e64006e71fb8f25f2b79f14524
Dennis Mesanovic <dmesa2@pdx.edu>
2019-08-12 18:44:30 -0700

    Update LogIn.js

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,7 @@
 //The tree background image is taken from this open source website: https://www.pexels.com/photo/countryside-dawn-daylight-environment-286305/
 
 import React, { Component } from 'react';
-import {  View, Text, TouchableOpacity, ImageBackground, TextInput, TouchableHighlight} from 'react-native';
+import { ActivityIndicator, View, Text, TouchableOpacity, ImageBackground, TextInput, TouchableHighlight} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Header from './header';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -16,65 +16,98 @@ class LogIn extends Component {
     this.state = { 
         email: '',
         username: '',
-        password: ''
+        password: '',
+        loading: true
     };
   }
 
-  Authenticate = async () => {
+  componentDidMount() {
+    //alert('rawr');
+    (async () => {
+      //await AsyncStorage.removeItem('userObject');
+      let user = await AsyncStorage.getItem('userObject');
+      if(user) {
+        Actions.Main({username: user.name})
+      } else {
+        this.setState({
+          loading: false
+        });
+      }
+    })();
+  }
+
+  Authenticate = () => {
+    //check if fields are populated
+    if(!this.state.email || !this.state.username || !this.state.password) {
+      alert('Please fill out the form');
+      return;
+    }
     let user = {
       email: this.state.email,
       name: this.state.username,
       hash: '',
       id: -1
     };
-    let res, verified;
+    let res, verified = false;
 
     //ask backend if email already in use
-    res = await fetch('http://localhost:8765/user_info', {
+    res = fetch('http://159.65.97.145:8765/user_info', {
         headers: {
             'content-type': 'application/json',
             Accept: 'application/json'
         },
         method: 'POST',
         body: JSON.stringify(user)
-    });
-    res = await res.json();
-      
-    //if user email exists, verify that the password is correct
-    if(res.id != -1) {
-      //check the hash
-      verified = await bcrypt.compare(this.state.password, res.hash);
-      user.id = res.id
+    })
+    .then(res => res.json())
+    .then(res => {
+      //if user email exists, verify that the password is correct
+      if(res.id != -1) {
+        //check the hash
+        verified = bcrypt.compareSync(this.state.password, res.hash);
+        user.id = res.id
+        //If all good, move on to other screens
+        if(verified) {
+          AsyncStorage.setItem('userObject', JSON.stringify(user));
+          Actions.Main();
+        } else {
+          alert('Invalid login, try again');
+        }
 
-    //otherwise add the new user
-    } else {
-      verified = true;
-      user.hash = await bcrypt.hash(this.state.password, 10);
-      res = await fetch('http://localhost:8765/new_user', {
-        headers: {
-            'content-type': 'application/json',
-            Accept: 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(user)
-      });
-      res = await res.json();
-      if(res.id == -1) {
-        verified = false;
+
+      //otherwise add the new user
+      } else {
+        verified = true;
+        user.hash = bcrypt.hashSync(this.state.password, 10);
+        res = fetch('http://159.65.97.145:8765/new_user', {
+          headers: {
+              'content-type': 'application/json',
+              Accept: 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(res => {
+          if(res.id == -1) {
+            verified = false;
+          }
+          user.id = res.id;
+          //If all good, move on to other screens
+          if(verified) {
+            AsyncStorage.setItem('userObject', JSON.stringify(user));
+            Actions.Main({username: user.name});
+          } else {
+            alert('Invalid login, try again');
+          }
+
+        });
       }
-      user.id = res.id;
-    }
-
-    //If all good, move on to other screens
-    if(verified) {
-      AsyncStorage.setItem('userObject', JSON.stringify(user));
-      Actions.Launch();
-    } else {
-      alert('Invalid login, try again');
-    }
+    });
   }
 
   render() {
+    if(!this.state.loading)
     return (
       <ImageBackground source={require('bookbranch/img/dawn-daylight.jpg')} style={{width: '100%', height: '100%'}}>
         <View>
@@ -113,6 +146,13 @@ class LogIn extends Component {
         </View>
       </ImageBackground>
     );
+    else
+      return(
+            <View style={{flex: 1, padding: 20}}>
+                <ActivityIndicator/>
+            </View>
+        );
+    render
   }
 }
 
